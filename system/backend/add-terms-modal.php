48a963e5dc2a25630187d9e96b284b0ffede692d
Navskie <85003094+Navskie@users.noreply.github.com>
2022-09-15 04:16:38 +0800

    test official statement

--- system/backend/add-terms-modal.php
+++ system/backend/add-terms-modal.php
@@ -2,13 +2,13 @@
     <div class="modal-dialog modal-dialog-centered">
     <div class="modal-content">
         <div class="modal-header">
-            <h4 class="modal-title text-center"><label for="">YOUR DIRECT MAIL BOX OPTION</label></h4>
+            <h4 class="modal-title text-center"><label for="">OFFICIAL STATEMENT</label></h4>
         </div>
         <div class="modal-body">
         <form action="backend/add-terms-process.php?terms=<?php echo $get_transaction_fetch['trans_poid']; ?>" method="post">
             <div class="row">
                 <div class="col-12">
-                    <img src="../images/statement.jpg" alt="" class="img-responsive w-100">
+                    <img src="./images/statement.jpg" alt="" class="img-responsive w-100">
                 </div>
             </div>
         </div>
