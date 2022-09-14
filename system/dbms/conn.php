fb9eba4ae7e9e9c61bb66b946d0f5d47c9ad6051
Navskie <85003094+Navskie@users.noreply.github.com>
2022-09-14 20:07:14 +0800

    db

--- system/dbms/conn.php
+++ system/dbms/conn.php
@@ -1,5 +1,5 @@
 <?php
     
-    // $connect = mysqli_connect('localhost', 'u708090748_upti', '@User2022', 'u708090748_beta');
-    $connect = mysqli_connect('localhost', 'root', '', 'uptimisedph');
+    $connect = mysqli_connect('localhost', 'u708090748_uptimised', '@User2022', 'u708090748_uptimisedph'); 
+    // $connect = mysqli_connect('localhost', 'root', '', 'uptimisedph');
 ?>
