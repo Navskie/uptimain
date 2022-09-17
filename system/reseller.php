eff33e611b24502a25cfc4e19c0baa3756916809
Navskie <85003094+Navskie@users.noreply.github.com>
2022-09-17 11:12:29 +0800

    test 2

--- system/reseller.php
+++ system/reseller.php
@@ -164,6 +164,191 @@
       </div>
     </div>
     <hr><br>
+    <div class="row">
+      <?php
+        if ($emp_position != '') {
+          $total = "SELECT COUNT(trans_poid) AS total FROM upti_transaction WHERE trans_my_reseller = '$myid'";
+          $total_sql = mysqli_query($connect, $total);
+          $total_fetch = mysqli_fetch_array($total_sql);
+        } else {
+          $total = "SELECT COUNT(trans_poid) AS total FROM upti_transaction WHERE trans_seller = '$myid' OR trans_my_reseller = '$myid'";
+          $total_sql = mysqli_query($connect, $total);
+          $total_fetch = mysqli_fetch_array($total_sql);
+        }
+      ?>
+      <div class="col-lg-3 col-md-6 col-sm-12">
+        <div class="course">
+          <div class="preview bg-warning">
+            <h2 class="text-center"><i class="uil uil-shopping-cart-alt"></i></h2>
+          </div>
+
+          <div class="info">
+            <h6>TOTAL ORDERS</h6>
+            <h2><b><?php echo $total_fetch['total'] ?></b></h2>
+            <a href="my-order.php" class="text-info">MORE INFO </a>
+          </div>
+        </div>
+        <br>
+      </div>
+
+      <?php
+        if ($emp_position != '') {
+          $pending = "SELECT COUNT(trans_poid) AS total FROM upti_transaction WHERE trans_my_reseller = '$myid' AND trans_status = 'Pending'";
+          $pending_sql = mysqli_query($connect, $pending);
+          $pending_fetch = mysqli_fetch_array($pending_sql);
+        } else {
+          $pending = "SELECT COUNT(trans_poid) AS total FROM upti_transaction WHERE trans_seller = '$myid' AND trans_status = 'Pending'";
+          $pending_sql = mysqli_query($connect, $pending);
+          $pending_fetch = mysqli_fetch_array($pending_sql);
+        }
+      ?>
+      <div class="col-lg-3 col-md-6 col-sm-12">
+        <div class="course">
+          <div class="preview bg-secondary">
+            <h2 class="text-center"><i class="uil uil-clock-five"></i></h2>
+          </div>
+
+          <div class="info">
+            <h6>PENDING ORDERS</h6>
+            <h2><b><?php echo $pending_fetch['total'] ?></b></h2>
+            <a href="order-pending.php" class="text-info">MORE INFO </a>
+          </div>
+        </div>
+        <br>
+      </div>
+
+      <?php
+        if ($emp_position != '') {
+          $process = "SELECT COUNT(trans_poid) AS total FROM upti_transaction WHERE trans_my_reseller = '$myid' AND trans_status = 'On Process'";
+          $process_sql = mysqli_query($connect, $process);
+          $process_fetch = mysqli_fetch_array($process_sql);
+        } else {
+          $process = "SELECT COUNT(trans_poid) AS total FROM upti_transaction WHERE trans_seller = '$myid' AND trans_status = 'On Process'";
+          $process_sql = mysqli_query($connect, $process);
+          $process_fetch = mysqli_fetch_array($process_sql);
+        }
+      ?>
+      <div class="col-lg-3 col-md-6 col-sm-12">
+        <div class="course">
+          <div class="preview bg-info">
+            <h2 class="text-center"><i class="uil uil-process"></i></h2>
+          </div>
+
+          <div class="info">
+            <h6>ON PROCESS ORDERS</h6>
+            <h2><b><?php echo $process_fetch['total'] ?></b></h2>
+            <a href="order-on-process.php" class="text-info">MORE INFO </a>
+          </div>
+        </div>
+        <br>
+      </div>
+
+      <?php
+        if ($emp_position != '') {
+          $transit = "SELECT COUNT(trans_poid) AS total FROM upti_transaction WHERE trans_my_reseller = '$myid' AND trans_status = 'In Transit'";
+          $transit_sql = mysqli_query($connect, $transit);
+          $transit_fetch = mysqli_fetch_array($transit_sql);
+        } else {
+          $transit = "SELECT COUNT(trans_poid) AS total FROM upti_transaction WHERE trans_seller = '$myid' AND trans_status = 'In Transit'";
+          $transit_sql = mysqli_query($connect, $transit);
+          $transit_fetch = mysqli_fetch_array($transit_sql);
+        }
+      ?>
+      <div class="col-lg-3 col-md-6 col-sm-12">
+        <div class="course">
+          <div class="preview bg-primary">
+            <h2 class="text-center"><i class="uil uil-truck"></i></h2>
+          </div>
+
+          <div class="info">
+            <h6>IN TRANSIT ORDERS</h6>
+            <h2><b><?php echo $transit_fetch['total'] ?></b></h2>
+            <a href="order-on-transit.php" class="text-info">MORE INFO </a>
+          </div>
+        </div>
+        <br>
+      </div>
+
+      <?php
+        if ($emp_position != '') {
+          $delivered = "SELECT COUNT(trans_poid) AS total FROM upti_transaction WHERE trans_my_reseller = '$myid' AND trans_status = 'Delivered'";
+          $delivered_sql = mysqli_query($connect, $delivered);
+          $delivered_fetch = mysqli_fetch_array($delivered_sql);
+        } else {
+          $delivered = "SELECT COUNT(trans_poid) AS total FROM upti_transaction WHERE trans_seller = '$myid' AND trans_status = 'Delivered'";
+          $delivered_sql = mysqli_query($connect, $delivered);
+          $delivered_fetch = mysqli_fetch_array($delivered_sql);
+        }
+      ?>
+      <div class="col-lg-3 col-md-6 col-sm-12">
+        <div class="course">
+          <div class="preview bg-success">
+            <h2 class="text-center"><i class="uil uil-check-circle"></i></h2>
+          </div>
+
+          <div class="info">
+            <h6>DELIVERED ORDERS</h6>
+            <h2><b><?php echo $delivered_fetch['total'] ?></b></h2>
+            <a href="order-delivered.php" class="text-info">MORE INFO </a>
+          </div>
+        </div>
+        <br>
+      </div>
+
+      <?php
+        if ($emp_position != '') {
+          $canceled = "SELECT COUNT(trans_poid) AS total FROM upti_transaction WHERE trans_my_reseller = '$myid' AND trans_status = 'Canceled'";
+          $canceled_sql = mysqli_query($connect, $canceled);
+          $canceled_fetch = mysqli_fetch_array($canceled_sql);
+        } else {
+          $canceled = "SELECT COUNT(trans_poid) AS total FROM upti_transaction WHERE trans_seller = '$myid' AND trans_status = 'Canceled'";
+          $canceled_sql = mysqli_query($connect, $canceled);
+          $canceled_fetch = mysqli_fetch_array($canceled_sql);
+        }
+      ?>
+      <div class="col-lg-3 col-md-6 col-sm-12">
+        <div class="course">
+          <div class="preview bg-danger">
+            <h2 class="text-center"><i class="uil uil-times-circle"></i></h2>
+          </div>
+
+          <div class="info">
+            <h6>CANCELED ORDERS</h6>
+            <h2><b><?php echo $canceled_fetch['total'] ?></b></h2>
+            <a href="order-cancel.php" class="text-info">MORE INFO </a>
+          </div>
+        </div>
+        <br>
+      </div>
+
+      <?php
+        if ($emp_position != '') {
+          $rts = "SELECT COUNT(trans_poid) AS total FROM upti_transaction WHERE trans_my_reseller = '$myid' AND trans_status = 'RTS'";
+          $rts_sql = mysqli_query($connect, $rts);
+          $rts_fetch = mysqli_fetch_array($rts_sql);
+        } else {
+          $rts = "SELECT COUNT(trans_poid) AS total FROM upti_transaction WHERE trans_seller = '$myid' AND trans_status = 'RTS'";
+          $rts_sql = mysqli_query($connect, $rts);
+          $rts_fetch = mysqli_fetch_array($rts_sql);
+        }
+      ?>
+      <div class="col-lg-3 col-md-6 col-sm-12">
+        <div class="course">
+          <div class="preview bg-danger">
+            <h2 class="text-center"><i class="uil uil-corner-up-left-alt"></i></h2>
+          </div>
+
+          <div class="info">
+            <h6>RTS ORDERS</h6>
+            <h2><b><?php echo $rts_fetch['total'] ?></b></h2>
+            <a href="order-rts.php" class="text-info">MORE INFO </a>
+          </div>
+        </div>
+        <br>
+      </div>
+
+    </div>
+    <!-- /.row -->
       </div><!-- /.container-fluid -->
     </div>
     <!-- /.content-header -->
