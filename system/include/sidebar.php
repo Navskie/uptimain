2c9bca797be31f8759de7d899a2da2bc5f25e873
Navskie <85003094+Navskie@users.noreply.github.com>
2022-09-23 18:35:19 +0800

    stocks per country

--- system/include/sidebar.php
+++ system/include/sidebar.php
@@ -1501,6 +1501,14 @@
               </p>
             </a>
           </li>
+          <li class="nav-item">
+            <a href="s_ph.php" class="nav-link">
+              <i class="nav-icon uil uil-globe"></i>
+              <p>
+                Stocks
+              </p>
+            </a>
+          </li>
         </ul>
       </nav>
       <!-- /.sidebar-menu -->
---
+++ system/s_tw.php
@@ -0,0 +1,81 @@
+<?php include 'include/header.php'; ?>
+<?php include 'include/preloader.php'; ?>
+<?php include 'include/navbar.php'; ?>
+<?php include 'include/sidebar.php'; ?>
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper" style="background: #f8f8f8 !important">
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+        <div class="container-fluid">
+            <div class="card rounded-0">
+                <div class="card-body login-card-body text-dark">
+                    <div class="row">
+                        <div class="col-12">
+                        <span class="float-left text-primary"><b>TAIWAN STOCKS</b></span>
+
+                        <a href="s_kr.php" class="btn btn-primary btn-sm rounded-0 float-right">
+                          KOREA
+                        </a>
+                        <a href="s_tw.php" class="btn btn-primary btn-sm rounded-0 float-right">
+                          TAIWAN
+                        </a>
+                        <a href="s_jp.php" class="btn btn-primary btn-sm rounded-0 float-right">
+                          JAPAN
+                        </a>
+                        <a href="s_cnd.php" class="btn btn-primary btn-sm rounded-0 float-right">
+                          CANADA
+                        </a>
+                        <a href="s_sg.php" class="btn btn-primary btn-sm rounded-0 float-right">
+                          SINGAPORE
+                        </a>
+                        <a href="s_hk.php" class="btn btn-primary btn-sm rounded-0 float-right">
+                          HONGKONG
+                        </a>
+                        <a href="s_uae.php" class="btn btn-primary btn-sm rounded-0 float-right">
+                          DUBAI
+                        </a>
+                        <a href="s_mc.php" class="btn btn-primary btn-sm rounded-0 float-right">
+                          MACAU
+                        </a>
+                        </div>
+                        
+                        <div class="col-12">
+                            <hr>
+                        </div>
+                        <div class="col-12">
+                            <!-- Order List Table Start -->
+                            <table id="example22" class="table table-bordered">
+                                <thead>
+                                  <tr>
+                                    <th class="text-center">#</th>
+                                    <th colspan="2" class="text-center">Item Description</th>
+                                    <th colspan="2" class="text-center">Stocks</th>
+                                  </tr>
+                                </thead>
+                                <?php
+                                  $code_sql = "SELECT * FROM stockist_inventory WHERE si_item_country = 'TAIWAN' ORDER BY si_item_stock ASC";
+                                  $code_qry = mysqli_query($connect, $code_sql);
+                                  $number = 1;
+                                  while ($code = mysqli_fetch_array($code_qry)) {
+                                    $crtitical = $code['si_item_stock'];
+                                ?>
+                                <tr>
+                                  <td class="text-center" width="20"><?php echo $number; ?></td>
+                                  <td class="text-center" width="20"><b><?php echo $code['si_item_code'] ?></b></td>
+                                  <td class="text-center"><?php echo $code['si_item_desc'] ?></td> 
+                                  <td class="text-center"><?php echo $code['si_item_stock'] ?></td>
+                                </tr>
+                                <?php
+                                    $number++;
+                                  }
+                                ?>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content-header -->
+  </div>
+<?php include 'include/footer.php'; ?>
